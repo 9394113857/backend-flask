46782ea1e9a11ec2e358d0b217e6663a5b9c5af0
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2025-05-14 15:23:19 +0530

    Raghu Changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from datetime import datetime
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -6,7 +7,10 @@ app = Flask(__name__)
 # Define the default route
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Get the current date and time
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Return an HTML response with the date and time in bold
+    return f'Hello, World!<br><b>Current Date and Time: {now}</b>'
 
 # Check if the script is being run directly
 if __name__ == '__main__':
