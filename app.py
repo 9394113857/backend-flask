5feea59c3f8d464605cc2bb90eeb9294d06632b2
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2025-05-15 13:04:10 +0530

    Initial commit: Setup basic Flask project
    1. Initialized Git repository
    2. Added basic Flask app with a single route
    3. Created .gitignore to exclude unnecessary files
    
    Date:
    Time:

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+import pytz  # Add this import
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -7,10 +8,11 @@ app = Flask(__name__)
 # Define the default route
 @app.route('/')
 def hello_world():
-    # Get the current date and time
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Get the current date and time in IST
+    ist = pytz.timezone('Asia/Kolkata')
+    now = datetime.now(ist).strftime("%Y-%m-%d %H:%M:%S")
     # Return an HTML response with the date and time in bold
-    return f'Hello, World!<br><b>Current Date and Time: {now}</b>'
+    return f'Hello, World!<br><b>Current Date and Time (IST): {now}</b>'
 
 # Check if the script is being run directly
 if __name__ == '__main__':
